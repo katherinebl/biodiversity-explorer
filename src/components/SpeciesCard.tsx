9496a1f40884f9b4cada247b5049a49b212e5b89
Katherine Briceño <katherinebl@users.noreply.github.com>
2026-09-17 20:12:51 +0200

    style: restructure species card

--- src/components/SpeciesCard.tsx
+++ src/components/SpeciesCard.tsx
@@ -9,27 +9,37 @@ function SpeciesCard({ species }: SpeciesCardProps) {
   return (
     <>
       <h2>{species.canonicalName}</h2>
-      <p>{species.rank}</p>
-      <ul className="classification-list">
-        {species.classification.map((taxon) => (
-          <li key={taxon.key} className="classification-item">
-            <span className="classification-rank">{taxon.rank}</span>{" "}
-            <span className="classification-name">{taxon.name}</span>
-          </li>
-        ))}
-      </ul>
-      {species.conservationStatus && (
-        <p>Conservation Status: {species.conservationStatus}</p>
-      )}
-      {species.image && (
-        <div>
-          <img src={species.image.url} alt={species.canonicalName} />
-          <p>
-            Image License: {species.image.license} | Attribution:{" "}
-            {species.image.attribution}
-          </p>
+      <div className="species-card">
+        <div className="species-image-container">
+          {species.image && (
+            <div>
+              <img src={species.image.url} alt={species.canonicalName} />
+              <p>
+                Image License: {species.image.license} | Attribution:{" "}
+                {species.image.attribution}
+              </p>
+            </div>
+          )}
         </div>
-      )}
+
+        <div className="species-details-container">
+          <h3>TAXONOMY</h3>
+          <ul className="classification-list">
+            {species.classification.map((taxon) => (
+              <li key={taxon.key} className="classification-item">
+                <span className="classification-rank">{taxon.rank}</span>{" "}
+                <span className="classification-name">{taxon.name}</span>
+              </li>
+            ))}
+          </ul>
+          {species.conservationStatus && (
+            <>
+              <h3>CONSERVATION STATUS</h3>
+              <p>{species.conservationStatus}</p>
+            </>
+          )}
+        </div>
+      </div>
     </>
   );
 }
